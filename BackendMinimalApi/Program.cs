f25c97c8b30a9bc808edc3ce524aaa946cf450f9
buntad <dtawan.arunsawad@gmail.com>
2025-03-16 16:03:07 +0700

    Add DbContext

--- BackendMinimalApi/Program.cs
+++ BackendMinimalApi/Program.cs
@@ -1,4 +1,11 @@
+using BackendMinimalApi.Infrastructure.DBContext;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AuthConnection")));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
